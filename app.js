0c4cceee31dce00d3cbe816e1a3669842eb58725
SatananY <113567674+SatananY@users.noreply.github.com>
2023-02-12 17:01:54 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const ejs = require ("ejs")
+
+const mongoose = require ("mongoose")
+
+const app = express()
+app.use(express.static("public"))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/home",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/about",(req,res)=>{
+    res.render("about");
+});
+
+app.get("/contact",(req,res)=>{
+    res.render("contact");
+});
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running at port 3000");
+ })
