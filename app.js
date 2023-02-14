14e882d0e09f8498e37952a06e6edbb2bb822910
SatananY <satananyindee@gmail.com>
2023-02-14 23:15:20 +0700

    Update login signup
    Update

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.use(express.static("public"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
 
+mongoose.connect("mongodb+srv://Satnan:12345678my@cluster0.hwkkxo2.mongodb.net/UserDB?retryWrites=true&w=majority");
+
+const userSchema = {
+    username:String,
+    email: String,
+    password: String,
+    tel: String,
+    weight: String,
+    height: String,
+    radio_gender: String
+}
+
+const User = new mongoose.model("User", userSchema)
+
 app.get("/",(req,res)=>{
     res.render("home");
 });
@@ -33,6 +47,23 @@ app.get("/signup",(req,res)=>{
     res.render("signup");
 });
 
+app.post("/singup", (req, res) => {
+    const newUser = new User ({
+        email: req.body.email,
+        password: req.body.password,
+        username:req.body.username,
+        tel: req.body.tel,
+        weight: req.body.weight,
+        height: req.body.height,
+        radio_gender: req.body.radio_gender
+
+    });
+
+    newUser.save((err) => {
+        if (err) console.log (err)
+        else res.render("home")
+    })
+})
 
 app.listen(3000, ()=>{
     console.log("Server is running at port 3000");
