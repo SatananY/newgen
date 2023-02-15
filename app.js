59556a165759fe20a15d4c638d2fd20b351953eb
M Ron <mr.ronmaae@gmail.com>
2023-02-16 01:48:52 +0700

    80%
    11

--- app.js
+++ app.js
@@ -33,6 +33,25 @@ app.get("/signup",(req,res)=>{
     res.render("signup");
 });
 
+app.get("/more",(req,res)=>{
+    res.render("more");
+});
+
+app.get("/Eat",(req,res)=>{
+    res.render("Eat");
+});
+
+app.get("/Personalized",(req,res)=>{
+    res.render("Personalized");
+});
+
+app.get("/special",(req,res)=>{
+    res.render("special");
+});
+
+app.get("/Spacialadvice",(req,res)=>{
+    res.render("Spacialadvice");
+});
 
 app.listen(3000, ()=>{
     console.log("Server is running at port 3000");
